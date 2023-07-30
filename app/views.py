ce3b97c0f52b07eeadb6c42d9c8a2d1b11d0f5a8
shagufthaanjum20 <shagufthanjum13@gmail.com>
2023-07-30 19:19:05 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def data_render(request):
-    d={'name':'mahi','age':'4'}
+    d={'name':'mahi','age':15}
     return render(request,'data_render.html',context=d)
+
+
+def if_condition(request):
+    d={'a':10,'b':2000,'c':250}
+    return render(request,'ifconditons.html',context=d)    
